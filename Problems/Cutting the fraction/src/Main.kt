09bbc18384c4847dcafb90634478796b9b309dcb
Aras <amvitkus@gmail.com>
2020-11-24 13:25:09 -0500

    Completed some input and type conversion tasks.

---
+++ Problems/Cutting the fraction/src/Main.kt
@@ -0,0 +1,12 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+    // write your code here
+    //println("Test input: ")
+    
+    var something = scanner.nextDouble()
+
+    println(something.toLong())
+    
+}
