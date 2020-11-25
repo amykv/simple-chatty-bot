8181cc2f57890072f72da423be4cfb2e75d4572e
Aras <amvitkus@gmail.com>
2020-11-24 20:24:42 -0500

    Completed Stage 3.

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -17,6 +17,11 @@ fun main() {
     println("Enter remainders of dividing your age by 3, 5 and 7.")
 
     // reading all remainders
+    val ar1 = scanner.nextInt()
+    val ar2 = scanner.nextInt()
+    val ar3 = scanner.nextInt()
 
-    println("Your age is {yourAge}; that's a good time to start programming!")
+    val yourAge = (ar1 * 70 + ar2 * 21 + ar3 * 15) % 105
+
+    println("Your age is {$yourAge}; that's a good time to start programming!")
 }
