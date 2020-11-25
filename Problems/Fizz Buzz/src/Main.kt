36c7a09a59b9c600ca151b94b9b11789865d7b82
Aras <amvitkus@gmail.com>
2020-11-25 00:49:56 -0500

    Did FizzBuzz variation.

--- Problems/Fizz Buzz/src/Main.kt
+++ Problems/Fizz Buzz/src/Main.kt
@@ -4,4 +4,18 @@ fun main(args: Array<String>) {
     val scanner = Scanner(System.`in`)
 
     // write your code here
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
+
+    for (i in a..b) {
+        if (i % 3 == 0 && i % 5 == 0) {
+            println("FizzBuzz")
+        } else if (i % 5 == 0) {
+            println("Buzz")
+        } else if (i % 3 == 0) {
+            println("Fizz")
+        } else {
+            println(i)
+        }
+    }
 }
